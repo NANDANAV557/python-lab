5e4f3ccdd764a9f4aa354dec7eebf56443609514
NANDANAV557 <nandana27022005@gmail.com>
2025-10-13 16:16:43 +0530

    Add files via upload

---
+++ program13.py
@@ -0,0 +1,8 @@
+n = int(input("Enter an integer n: "))
+nn = n * 11
+n=int(input("Enter a number : "))
+s=(n+11*n+111*n)
+nnn = n * 111
+s = n + nn + nnn
+print(f"{n}+{nn} + {nnn} = {s}")
+print(f"{n}+{nn} + {nnn} = {s}")
