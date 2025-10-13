34b16943d7b7dc0cc15ca7f713ffa8f9fe298da8
NANDANAV557 <nandana27022005@gmail.com>
2025-10-13 14:39:05 +0530

    Add files via upload

---
+++ program3.py
@@ -0,0 +1,7 @@
+s=input("enter a sentence:")
+print(s)
+wordsList=S.split()
+print(wordslist)
+uniqueWords=set(wordList)
+for word in uniqueWords:
+    print(f"{word}occurs{wordList.count(word)}times")
